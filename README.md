3256c70e786188f1801017574e7fe303c0374f05
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2021-12-12 23:36:14 -0500

    updatedJS and README

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## Discription
 
 For this assignment/challenge I was asked to create an application that generates a random password using JavaScript. The HTML and CSS was already provided. 
+
+## Built With
+*JavaScript
+*HTML
+*CSS
+
+## Website
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,43 @@
 // Assignment code here
 
-function generatePassword () {
-alert("Please answer each question")
-var passwordCriteria = window.prompt ("Choose the length of your password? (8 to 128 characters)")
-var passwordNumber = window.confirm ("Would you like numbers in you password?");
-var passwardUpper= window.confirm ("Would you like uppercase letters in your password?");
-var passwardLower = window.confirm ("Would you like lowercase letters in your password?");
-var passwardSpecial = window.confirm ("Would you like special characters in you password?");
+var generatePassword = function () {
+  window.alert("Please answer each question")
+  var passwordCriteria = window.prompt ("Choose the length of your password? (8 to 128 characters)");
 
-console.log()
+  var passwordNumber = window.confirm ("Would you like numbers in you password?");
+  var randomNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  var passwordUpper= window.confirm ("Would you like uppercase letters in your password?");
+  var randomUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+  var passwordLower = window.confirm ("Would you like lowercase letters in your password?");
+  var randomLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var passwordSpecial = window.confirm ("Would you like special characters in you password?");
+  var randomSpecial = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+  var randomResult = [];
+  var passwordResult = [];
+
+
+  if (passwordNumber) {
+    randomResult = randomResult.concat(randomNumber)
+  } 
+  if (passwordUpper){
+    randomResult = randomResult.concat(randomUpper)
+  } 
+  if(passwordLower){
+    randomResult = randomResult.concat(randomLower)
+  } 
+  if(passwordSpecial){
+    randomSpecial = randomResult.concat(randomSpecial)
+  }
+
+  for (var i = 0; i < passwordCriteria; i++) {    
+    passwordResult.push(randomResult[Math.floor(Math.random() * randomResult.length)]); 
+  }
+
+  return passwordResult.join("");
 }
 
 
