10642095b6c649a65f8ec7f81846d4d96b5331b6
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2021-12-12 21:27:00 -0500

    added prompt and confirm windows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 // Assignment code here
 
+function generatePassword () {
+alert("Please answer each question")
+var passwordCriteria = window.prompt ("Choose the length of your password? (8 to 128 characters)")
+var passwordNumber = window.confirm ("Would you like numbers in you password?");
+var passwardUpper= window.confirm ("Would you like uppercase letters in your password?");
+var passwardLower = window.confirm ("Would you like lowercase letters in your password?");
+var passwardSpecial = window.confirm ("Would you like special characters in you password?");
+
+console.log()
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
